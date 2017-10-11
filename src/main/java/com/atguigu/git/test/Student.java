422b8376ee07b07bfc3a61d72b7735b6fd331e33
zztao_global <zztao_global@atguigu.com>
2017-10-11 20:33:31 +0800

    qwe

--- src/main/java/com/atguigu/git/test/Student.java
+++ src/main/java/com/atguigu/git/test/Student.java
@@ -1,5 +1,12 @@
 package com.atguigu.git.test;
 
 public class Student {
-
+	
+	public static void main(String[] args) {
+		System.out.println("asdfghjkl...................");
+		System.out.println("666666");
+		System.out.println("sddddd");
+		System.out.println("111111");
+	}
+	
 }
